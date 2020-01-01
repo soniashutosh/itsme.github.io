29e230b5233884d603f1a064ced98a61c1467839
Ashutosh Soni <46646804+ashutosh0699@users.noreply.github.com>
2020-01-01 07:37:51 +0530

    My First Website

---
+++ datafile.php
@@ -0,0 +1,12 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+$formcontent=" From: $name \n Phone: $phone \n Message: $message";
+$recipient = "ashutoshsoni0699@gmail.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail("ashutoshsoni0699@gmail", $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!" . " -" . "<a href='index.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+?>
